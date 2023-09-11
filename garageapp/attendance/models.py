a76ee931a5d2c8ded5b211ccc6fce3a4790e5f6e
tizan25 <tizan25@gmail.com>
2023-09-10 23:59:53 -0300

    First db models

--- garageapp/attendance/models.py
+++ garageapp/attendance/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    address = models.CharField(max_length=200)
+
+
+class RehearsalType(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+
+
+class Rehearsal(models.Model):
+    rehearsal_date = models.DateTimeField(name='fecha')
+    location = models.ForeignKey(Location, on_delete=models.RESTRICT)
+    rehearsal_type = models.ForeignKey(
+        RehearsalType, on_delete=models.RESTRICT)
+
+
+class ChoirSinger(models.Model):
+    id_number = models.IntegerField(primary_key=True, blank=False, unique=True)
+    first_name = models.CharField(max_length=200, blank=False)
+    last_name = models.CharField(max_length=200, blank=False)
+    date_of_birth = models.DateTimeField()
+
+
+class Attendance(models.Model):
+    singer = models.ForeignKey(ChoirSinger, on_delete=models.RESTRICT)
+    rehearsal = models.ForeignKey(Rehearsal, on_delete=models.RESTRICT)
